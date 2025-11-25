186130461bf7b593e5b59d9a5716a2df6e835d9d
郑在希 <12240093+zheng-zaixi@user.noreply.gitee.com>
2025-11-25 20:39:53 +0800

    配置全局日志定义

--- src/main/java/com/smart/www/exception/GloBalHandlerException.java
+++ src/main/java/com/smart/www/exception/GloBalHandlerException.java
@@ -5,9 +5,17 @@ import com.smart.www.util.ResultCodeEnum;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GloBalHandlerException {
+
+    // 单独处理静态资源未找到异常,不打印日志
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseBody
+    public void handleNoResourceFoundException(NoResourceFoundException e) {
+        // 静默处理,不打印日志
+    }
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
